1b3d6a307e6cb7e9eb9e302c486ed93522e45730
lukeharris3 <166353470+lukeharris3@users.noreply.github.com>
2024-05-08 09:06:49 -0600

    Add files via upload

---
+++ intro2React/App.jsx
@@ -0,0 +1,38 @@
+import { pupList } from "./data.js";
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [puppies] = useState(pupList);
+  const [featPupId, setFeatPupId] = useState(null);
+  () => {};
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  return (
+    <div className={"App"}>
+      <h1>Puppies</h1>
+      {featPupId && (
+        <div id={"ele"}>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
